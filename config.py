2d6f932c6071908e0055de3327fd2ab94d09df94
Ismael Barros <backend17@plataformaverde.com.br>
2022-07-27 02:02:56 -0300

    Add initial files

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from sqlite3 import SQLITE_ALTER_TABLE
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or '123456'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
